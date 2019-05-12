a5c88ff5bb9f91e57cdd8256368e7747700ebf5d
Binaya Rimal <rimal.binaya@gmail.com>
2019-05-11 21:10:16 -0700

    tested

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+//#1
+  describe("GET /", () => {
+
+//#2
+   it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+        done();
+      });
+    });
+
+  });
+});
