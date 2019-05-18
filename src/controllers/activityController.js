0308b717de65b34df004a35e48c7a4f986fb8f51
Binaya Rimal <rimal.binaya@gmail.com>
2019-05-17 18:56:17 -0700

    trace back

--- src/controllers/activityController.js
+++ src/controllers/activityController.js
@@ -2,12 +2,6 @@ module.exports = {
 actForm(req,res,next){
   res.render("activity/actPage")
 },
-createActivity(req, res, next){
-  User.findOne({where:{activity:req.user.activity}})
-  .then(act){
-    
-  }
 
-},
 
 }
